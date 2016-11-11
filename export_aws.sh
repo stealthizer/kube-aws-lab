a366ddeff11f8ad8a6508a4f7694688bf950e01d
Jordi Franco <jordi.franco@scmspain.com>
2016-11-11 13:44:15 +0100

    added tests to export_aws.sh so if all information is not exported it returns error runlevel

--- export_aws.sh
+++ export_aws.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 if [ "$#" -ne 1 ]; then
     echo "You must specify a valid amazon account from your credentials file"
-    break 
+    return 
 else
     account=$1
     account_data=`cat ~/.aws/credentials|grep -A4 $account`
     ACCOUNT_KEY_ID=`echo "$account_data"|grep aws_access_key_id|cut -d= -f2|xargs`
     SECRET_ACCESS_KEY=`echo "$account_data"|grep aws_secret_access_key|cut -d= -f2|xargs`
+    REGION=`echo "$account_data"|grep region|cut -d= -f2|xargs`
     export AWS_ACCESS_KEY_ID=$ACCOUNT_KEY_ID
     export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY
+    export AWS_DEFAULT_REGION=$REGION
     # TEST if the exported variables are present
-    if [[ ! -z "$AWS_ACCESS_KEY_ID" && ! -z "AWS_SECRET_ACCESS_KEY" ]]; then 
+    if [[ ! -z "$AWS_ACCESS_KEY_ID" && ! -z "AWS_SECRET_ACCESS_KEY" && ! -z "AWS_DEFAULT_REGION" ]]; then 
         echo -n "$account credentials exported"
-        exit 0
     else 
         echo -n "account variables not set"
         exit 1
